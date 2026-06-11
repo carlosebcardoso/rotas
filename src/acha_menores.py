a0bda4799cd6bf4cc98a33696bd5d856f71335b4
Ciro C F <136134373+Koi000Neko@users.noreply.github.com>
2026-06-11 15:20:46 -0300

    Update acha_menores.py

--- src/acha_menores.py
+++ src/acha_menores.py
@@ -1,26 +1,26 @@
-def acha_menores(g, vOrigem):
-  menoresCustos = {v: float('inf') for v in g}
-  menoresCustos[vOrigem] = 0
+def acha_menores(g, v_origem):
+  menores_custos = {v: float('inf') for v in g}
+  menores_custos[v_origem] = 0
   verificados = set()
 
   while len(verificados) < len(g):
-    vAtual = None
-    menorCusto = float('inf')
+    v_atual = None
+    menor_custo = float('inf')
 
     for v in g:
-      if v not in verificados and menoresCustos[v] < menorCusto:
-        menorCusto = menoresCustos[v]
-        vAtual = v
+      if v not in verificados and menores_custos[v] < menor_custo:
+        menor_custo = menores_custos[v]
+        v_atual = v
     
-    if vAtual is None:
+    if v_atual is None:
       break
 
-    verificados.add(vAtual)
+    verificados.add(v_atual)
 
-    for vDestino, custo in g[vAtual]:
-      novoCusto = menoresCustos[vAtual] + custo
+    for v_destino, custo in g[v_atual]:
+      novo_custo = menores_custos[v_atual] + custo
 
-      if novoCusto < menoresCustos[vDestino]:
-        menoresCustos[vDestino] = novoCusto
+      if novo_custo < menores_custos[v_destino]:
+        menores_custos[v_destino] = novo_custo
 
-  return menoresCustos
+  return menores_custos
