c1c5c0e0c4b29e31fcc7a6b71c27f2b5fa7b577c
Ciro C F <136134373+Koi000Neko@users.noreply.github.com>
2026-06-11 13:28:32 -0300

    Update and rename PegaMenor.py to AchaMenores.py

---
+++ src/AchaMenores.py
@@ -0,0 +1,26 @@
+def AchaMenores(g, vOrigem):
+  menoresCustos = {v: float('inf') for v in g}
+  menoresCustos[vOrigem] = 0
+  verificados = set()
+
+  while len(verificados) < len(g):
+    vAtual = None
+    menorCusto = float('inf)
+
+    for v in g:
+      if v not in verificados and menoresCustos[v] < menorCusto:
+        menorCusto = menoresCustos[v]
+        vAtual = v
+    
+    if vAtual is None:
+      break
+
+    verificados.add(vAtual)
+
+    for vDestino, custo in g[vAtual]:
+      novoCusto = menoresCustos[vAtual] + custo
+
+      if novoCusto < menoresCustos[vDestino]:
+        menoresCustos[vDestino] = novoCusto
+
+  return menoresCustos
